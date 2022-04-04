6d936e0d01c778113e9894796483dfccfbaa00d7
unknown <calphonce31@gmail.com>
2022-04-05 01:21:47 +0300

    first commit

---
+++ js/home.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.getElementById("searchInput").onclick =() =>{
+    document.getElementById("public").style.display ="block";
+    document.getElementById("lineOne").style.display ="block";
+}
