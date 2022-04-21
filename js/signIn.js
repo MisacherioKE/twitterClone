b724ce28464c1729d16187298038e535fbddb758
Carlos Misachi <calphonce31@gmail.com>
2022-04-21 12:13:07 +0300

    signInWithGoogle

--- js/signIn.js
+++ js/signIn.js
@@ -56,3 +56,45 @@ document.getElementById("signIn").onclick =()=>{
 document.getElementById("resetWindow").onclick =()=>{
     window.location.href ="/html/resetPass.html";
 }
+// Sign in with Google
+// import { GoogleAuthProvider } from "firebase/auth";
+
+// const provider = new GoogleAuthProvider();
+
+
+// import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+// const auth = getAuth();
+document.getElementById("google").onclick = ()=>{
+    // const auth = getAuth();
+    // let provider = new GoogleAuthProvider();
+    let provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+   firebase.auth().signInWithPopup(provider)
+    
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var credential = result.credential
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        window.location.href ="/html/home.html";
+      }).catch((error) => {
+          alert(error.message);
+        // Handle Errors here.
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // // The email of the user's account used.
+        // const email = error.email;
+        // // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+    
+}
+
+//   import { getAuth, signInWithRedirect } from "firebase/auth";
+
+// const auth = getAuth();
+// signInWithRedirect(auth, provider);
