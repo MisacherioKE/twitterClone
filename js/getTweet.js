6f2690c9ca14e1244b7737a017e16a2b6cd8f5af
Carlos Misachi <calphonce31@gmail.com>
2022-04-26 13:15:52 +0300

    edit features

--- js/getTweet.js
+++ js/getTweet.js
@@ -12,31 +12,71 @@ firebase.auth().onAuthStateChanged((user)=>{
             querySnapshot.forEach((doc)=>{
                 let tweet = doc.data().tweet;
                 let time = doc.data().timestamp;
-                let newTime = time.toDate();
+                let newTime = time.toDate().toDateString();
                 let name = doc.data().username;
+                let tweetId = doc.data().tweetId
       
                 if(doc.exists){
 
-                    let content = '';
-                    content +=`<div>`
-                    content +=`<p>${name}</p>`
-                    content +=`<p>${newTime}</p>`
-                    content +=`<p>${tweet}</p>`
-                    content +=`<button id="edit">Edit</button>`
-                    content +=`</div>`
+                    // let content = '';
+                    // content +=`<div>`
+                    // content +=`<p>${name}</p>`
+                    // content +=`<p>${newTime}</p>`
+                    // content +=`<p>${tweet}</p>`
+                    // content +=`<button id="edit">Edit</button>`
+                    // content +=`</div>`
                     
-                    $("#sectTwoB").append(content);
+                    // $("#sectTwoB").append(content);
+
+
+                    let input = '';
+                    input +=`<div>`
+                    input +=`<p>${name}</p>`
+                    input +=`<p>${newTime}</p>`
+                    input += `<p>${tweet}</p>`
+                    input +=`<i onClick ="editTweet"(\`${tweetId}\`) class="fa-solid fa-pen-to-square"></i>`
+                    input +=`<i class="fa-solid fa-trash"></i>`
+                    input +=`</div>`
+
+                    $("#edit").append(input);
+
                     
-                    document.getElementById("edit").onclick =()=>{
-                        let content ="";
-                        content += `<div>`
-                        content += `<p>${tweet}</p>`
-                        content += `<button id="update">update</button>`
-                        content += `<button id="delete">delete</button>`
-                        content += `</div>`
-                        $("#editBox").append(content);
-                        document.getElementById("editBox").style.display ="block";
+                    window.editTweet = (value) =>{
+
+                        // Invoke fiorebase
+                        firebase.firestore().collection("tweets").doc(value)
+                        .get().then((doc)=>{
+                            let tweet = doc.data().tweet;
+                            document.getElementById("newtweet").value = tweet;
+                        })
+
+                        // Edit tweet
+                        document.getElementById("update").onclick =()=>{
+                            let newTweet = document.getElementById("newtweet").value;
+
+                            // Invoking firebase
+                            firebase.firestore().collection("tweets").doc(value)
+                            .update({
+                                tweet:newTweet
+                            }).then(()=>{
+                                alert("Tweet updated successfully");
+                                window.location.reload();
+                            }).catch((error)=>{
+                                alert(error.message);
+                            })
+                        }
                     }
+                    // document.getElementById("edit").onclick =()=>{
+                    //     let content ="";
+                    //     content += `<div>`
+                    //     content += `<p>${tweet}</p>`
+                    //     content += `<button id="update">update</button>`
+                    //     content += `<button id="delete">delete</button>`
+                    //     content += `</div>`
+                    //     $("#editBox").append(content);
+                    //     document.getElementById("editBox").style.display ="block";
+                    // }
+
                 }else{
                     alert("There's no such document");
                 }
