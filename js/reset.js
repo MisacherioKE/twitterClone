aab9c3a6f3029046e2bc522c28c95cd5795f0c00
Carlos Misachi <calphonce31@gmail.com>
2022-04-28 19:35:44 +0300

    updates

--- js/reset.js
+++ js/reset.js
@@ -5,48 +5,51 @@ window.oncontextmenu = ()=>{
 }
 
 
-firebase.firestore().collection("users").doc()
+firebase.firestore().collection("users")
 .get().then((querySnapshot)=>{
     querySnapshot.forEach((doc)=>{
         let email = doc.data().email;
-    }
+        document.getElementById("email").value = email;
+
+        document.getElementById("reset").onclick = ()=>{
+   
+             let email = document.getElementById("email").value; 
+           
+            if(!email){
+                alert("Empty field!")
+                document.getElementById("email").style.border ="solid red";
+            }else{
+                document.getElementById("email").style.border ="solid gray";
+                document.getElementById("loading").style.display ="block";
+                document.getElementById("reset").style.display ="none";   
+            }
+            //       // invoke firebase
+                  firebase.auth().sendPasswordResetEmail(email).then(()=>{
+                    alert("A reset link has been sent to your email");
+                    alert("please login with your new password");
+                    window.location.href ="/index.html";
+                }).catch((error) =>{
+                    alert(error.message);
+                    console.log(error.message);
+                    document.getElementById("loading").style.display ="none";
+                    document.getElementById("reset").style.display ="block";
+                })
+        
+            // invoke firebase
+            // firebase.auth().sendPasswordResetEmail(email).then(()=>{
+            //     alert("A reset link has been sent to your email");
+            // }).catch((error) =>{
+            //     alert(error.message);
+        
+            //     console.log(error.message);
+            // })
+        }
+    })
    
   
 })
 
-document.getElementById("reset").onclick = ()=>{
-   
-     let email = document.getElementById("email").value; 
-   
-    if(!email){
-        alert("Empty field!")
-        document.getElementById("email").style.border ="solid red";
-    }else{
-        document.getElementById("email").style.border ="solid gray";
-        document.getElementById("loading").style.display ="block";
-        document.getElementById("reset").style.display ="none";   
-    }
-          // invoke firebase
-          firebase.auth().sendPasswordResetEmail(email).then(()=>{
-            alert("A reset link has been sent to your email");
-            alert("please login with your new password");
-            window.location.href ="/index.html";
-        }).catch((error) =>{
-            alert(error.message);
-            console.log(error.message);
-            document.getElementById("loading").style.display ="none";
-            document.getElementById("reset").style.display ="block";
-        })
 
-    // invoke firebase
-    // firebase.auth().sendPasswordResetEmail(email).then(()=>{
-    //     alert("A reset link has been sent to your email");
-    // }).catch((error) =>{
-    //     alert(error.message);
-
-    //     console.log(error.message);
-    // })
-}
 
 // Close Button
 document.getElementById("close").onclick =()=>{
