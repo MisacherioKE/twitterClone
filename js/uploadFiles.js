8594b321bdca663742c1d4b87485e941309e4aec
Carlos Misachi <calphonce31@gmail.com>
2022-05-10 23:28:07 +0300

    chatting

--- js/uploadFiles.js
+++ js/uploadFiles.js
@@ -15,19 +15,19 @@ firebase.auth().onAuthStateChanged((user)=>{
             // get file
             let file = document.getElementById("files").files[0];
 
-            console.log(file);
             let uploadimg = storageRef.child("profilePics/").child(file.name).put(file);
 
             uploadimg.on("state_changed", (snapshot) =>{
                 var progress = Math.floor(snapshot.bytesTransferred/snapshot.totalBytes)*100 ;
                 document.getElementById("uploadProgress").innerHTML = progress;
-                console.log(progress);
+                document.getElementById("filename").innerHTML = file.name;
+               
 
             }, (error) =>{
                 alert(error.message);
             }, ()=>{
                 uploadimg.snapshot.ref.getDownloadURL().then((downloadURL)=>{
-                    console.log(downloadURL);
+                    
                     firebase.firestore().collection("users").doc(user.uid)
                     .update({
                         downloadURL:downloadURL
