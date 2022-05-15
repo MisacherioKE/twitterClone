5221a60aaef16bff5c6684802c981ea33b378c8b
Carlos Misachi <calphonce31@gmail.com>
2022-05-15 10:50:31 +0300

    updates

--- js/query.js
+++ js/query.js
@@ -21,8 +21,8 @@ firebase.auth().onAuthStateChanged((user)=>{
 
 
                  let content = "";
-                 content +=`<div>`
-                 content +=`<h5>@${name}</h5>`
+                 content +=`<div style="position: relative;background-color: whitesmoke;width: 400px;height: fit-content;">`
+                 content +=`<h5 style="position: absolute;top: 12%;left: 18%;">@${name}</h5>`
                  content +=`<img src="${downloadURL}" alt="" class="img" style="height: 50px; object-fit: cover; border-radius: 50%; width: 50px; margin-left: 1%; margin-bottom: 15%;">`
                  content +=`<h5>${email}</h5>`
                  content +=`<h5>${phone}</h5>`
